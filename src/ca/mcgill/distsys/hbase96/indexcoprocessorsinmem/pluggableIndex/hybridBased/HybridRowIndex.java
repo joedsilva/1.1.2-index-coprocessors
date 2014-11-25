714fe5951782f44104b13dc178416616ad8e680a
myahmad <myahmad0@gmail.com>
2014-11-25 14:03:32 -0500

    Fixed whitespaces.

--- src/ca/mcgill/distsys/hbase96/indexcoprocessorsinmem/pluggableIndex/hybridBased/HybridRowIndex.java
+++ src/ca/mcgill/distsys/hbase96/indexcoprocessorsinmem/pluggableIndex/hybridBased/HybridRowIndex.java
@@ -10,65 +10,63 @@ import ca.mcgill.distsys.hbase96.indexcommonsinmem.ByteUtil;
 
 public class HybridRowIndex implements Comparable<HybridRowIndex> ,  Serializable{
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 5706149244338474248L;
-	private byte [] rowKey;
-	private TreeSet<byte[]> pkRefs;
-	private transient ReentrantReadWriteLock rwLock;
-	
-	
-	public HybridRowIndex(byte [] rowKey){
-		pkRefs = new TreeSet<byte[]>(ByteUtil.BYTES_COMPARATOR);
+  /**
+   *
+   */
+  private static final long serialVersionUID = 5706149244338474248L;
+  private byte [] rowKey;
+  private TreeSet<byte[]> pkRefs;
+  private transient ReentrantReadWriteLock rwLock;
+
+
+  public HybridRowIndex(byte [] rowKey){
+    pkRefs = new TreeSet<byte[]>(ByteUtil.BYTES_COMPARATOR);
     rwLock = new ReentrantReadWriteLock(true);
     this.rowKey = Bytes.copy(rowKey);
-	} 
-	
-	public TreeSet<byte[]> getPKRefs(){
-		return pkRefs;
-	}
-	
-	protected byte [] [] getPKRefsAsArray(){
-		rwLock.readLock().lock();
-      try {
-        if(pkRefs != null){
-          return pkRefs.toArray(new byte[pkRefs.size()][]);
-        } else {
-          return null;
-        }
-      } finally {
-        rwLock.readLock().unlock();
+  }
+
+  public TreeSet<byte[]> getPKRefs(){
+    return pkRefs;
+  }
+
+  protected byte [] [] getPKRefsAsArray(){
+    rwLock.readLock().lock();
+    try {
+      if(pkRefs != null){
+        return pkRefs.toArray(new byte[pkRefs.size()][]);
+      } else {
+        return null;
       }
-	}
-	
-	public byte[] getRowKey(){
-		return rowKey;
-	}
-	
-	public void add(byte[] value){
-		rwLock.writeLock().lock();
-		pkRefs.add(value);
-		rwLock.writeLock().unlock();
-	}
-	
-	public void remove(byte[] value) {
-		rwLock.writeLock().lock();
-		pkRefs.remove(value);
-		rwLock.writeLock().unlock();
-	}
+    } finally {
+      rwLock.readLock().unlock();
+    }
+  }
 
-	@Override
-	public int compareTo(HybridRowIndex arg0) {
-		// left > right, return positive. left == right, return 0
-		return Bytes.compareTo(this.rowKey, arg0.getRowKey());
-	}
-	
-	public static void main(String[] args) {
-		HybridRowIndex index1 = new HybridRowIndex(Bytes.toBytes("abc"));
-		HybridRowIndex index2 = new HybridRowIndex(Bytes.toBytes("ad"));
-		System.out.println(index2.compareTo(index1));
+  public byte[] getRowKey(){
+    return rowKey;
+  }
 
-	}
+  public void add(byte[] value){
+    rwLock.writeLock().lock();
+    pkRefs.add(value);
+    rwLock.writeLock().unlock();
+  }
 
+  public void remove(byte[] value) {
+    rwLock.writeLock().lock();
+    pkRefs.remove(value);
+    rwLock.writeLock().unlock();
+  }
+
+  @Override
+  public int compareTo(HybridRowIndex arg0) {
+    // left > right, return positive. left == right, return 0
+    return Bytes.compareTo(this.rowKey, arg0.getRowKey());
+  }
+
+  public static void main(String[] args) {
+    HybridRowIndex index1 = new HybridRowIndex(Bytes.toBytes("abc"));
+    HybridRowIndex index2 = new HybridRowIndex(Bytes.toBytes("ad"));
+    System.out.println(index2.compareTo(index1));
+  }
 }
